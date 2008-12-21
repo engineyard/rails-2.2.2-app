ba34f90a8a961944234c549f1fa6dc015382e188
Ezra Zygmuntowicz <ez@engineyard.com>
2008-12-20 18:04:41 -0800

    adding bench controller

---
+++ app/controllers/bench_controller.rb
@@ -0,0 +1,15 @@
+class BenchController < ApplicationController
+  
+  def index
+    render :text => "hello world"
+  end
+  
+  def stress
+    @items = []
+    5.times do
+      @items << Item.create(:title => 'hello world',
+                            :body  => 'this text is fake.' * 50)
+    end
+  end
+  
+end
