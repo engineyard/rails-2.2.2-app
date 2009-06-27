3603c4f3bd761e0b1e8b97b7464e5605570b450a
Ezra Zygmuntowicz <ez@engineyard.com>
2009-06-27 14:13:32 -0700

    symlink /shared to /data/app/current/public/s3

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -5,3 +5,5 @@ run "echo 'current_path: #{current_path}' >> #{shared_path}/logs.log"
 run "echo 'shared_path: #{shared_path}' >> #{shared_path}/logs.log"
 
 sudo "echo 'sudo works' >> /root/sudo.log"
+
+run "ln -nfs /shared #{current_path}/public/s3"
