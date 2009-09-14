fb1aba7929b0cc718fbc7d08f977d5289730724f
Jon Crosby <jon@joncrosby.me>
2009-09-14 14:45:10 -0700

    Make before_restart write out to a before_restart file

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,6 +1,6 @@
-run "echo \"RAILS_ENV: $RAILS_ENV\" >> #{release_path}/after_restart.log"
-run "echo \"shared_path: #{shared_path}\" >> #{release_path}/after_restart.log"
-run "echo \"current_path: #{current_path}\" >> #{release_path}/after_restart.log"
+run "echo \"RAILS_ENV: $RAILS_ENV\" >> #{release_path}/before_restart.log"
+run "echo \"shared_path: #{shared_path}\" >> #{release_path}/before_restart.log"
+run "echo \"current_path: #{current_path}\" >> #{release_path}/before_restart.log"
 sudo "echo 'sudo works' >> /root/sudo.log"
 
 run "ln -nfs /shared #{current_path}/public/s3"
