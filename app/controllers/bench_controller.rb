004282977c732c6f8053611f2f70a038cb9aa068
Brian Donovan <bdonovan@engineyard.com>
2010-03-17 12:28:58 -0700

    Give both the system ruby version and the running ruby version.

--- app/controllers/bench_controller.rb
+++ app/controllers/bench_controller.rb
@@ -1,9 +1,9 @@
 class BenchController < ApplicationController
-  
+
   def index
     render :text => "hello world"
   end
-  
+
   def stress
     @items = []
     5.times do
@@ -11,9 +11,10 @@ class BenchController < ApplicationController
                             :body  => 'this text is fake.' * 50)
     end
   end
-  
+
   def info
-    render :text => `ruby -v`
+    headers['Content-Type'] = 'text/plain'
+    render :text => "System Ruby:\n\n  $ ruby -v\n  #{`ruby -v`.chomp}\n\n\nRunning Ruby:\n\n  RUBY_VERSION => #{RUBY_VERSION}\n  RUBY_PLATFORM => #{RUBY_PLATFORM}"
   end
 
 end
